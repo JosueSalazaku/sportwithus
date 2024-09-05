222db214e53474997f0b6062174c053efa523e9b
Josué Salazaku <josuesalazaku@gmail.com>
2024-09-06 01:35:13 +0200

    fetch months data and display them

---
+++ src/Components/months.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../lib/supabaseClient'; 
+
+export default function Months() {
+    
+  interface Month {
+    id: number;
+    month: string;
+  }
+  
+  const [months, setMonths] = useState<Month[]>([]);
+
+  useEffect(() => {
+    getMonths();
+  }, []);
+
+  async function getMonths() {
+      const { data, error } = await supabase.from('months').select();
+      console.log(data);
+    if (error) {
+      console.error('Error fetching months:', error);
+    } else {
+      setMonths(data);
+    }
+  }
+
+  return (
+    <div>
+      <h1>Months</h1>
+      <ul>
+        {months.length > 0 ? (
+          months.map((month) => (
+            <li key={month.id}>
+              {month.month}
+            </li>
+          ))
+        ) : (
+          <p>No data available</p>
+        )}
+      </ul>
+    </div>
+  );
+}
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,16 @@
+import Months from "@/Components/months";
+import { Button } from "@/Components/ui/button";
+
 export default function Home() {
     return (
       <main className='w-screen h-screen flex justify-center items-center'>
         <div className='flex flex-col justify-center items-center space-y-6'>
           <h1 className='text-6xl font-black'>SPORT WITH US</h1>
           <p className="text-2xl font-medium">COMING SOON</p>
+          <Months/>
+          <Button>CLICK HER TO JOIN</Button>
         </div>
+
       </main>
     )
   }
